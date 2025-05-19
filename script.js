211b1a85bb623a5e2ae9cef1175e295f392108c5
Pablo <pablogroza2000@gmail.com>
2025-05-19 22:56:29 +0200

    Finished HTML, finishing CSS, starting with Javacript

---
+++ script.js
@@ -0,0 +1,4 @@
+const passwordGen = document.querySelector(".password")
+const copyPass = document.querySelector(".copy")
+const btn = document.querySelector(".btn-box")
+const copiedPass = document.querySelector(".copied")

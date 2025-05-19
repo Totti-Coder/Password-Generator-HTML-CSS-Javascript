2e5a978d0f7d98d44379eab01f16336634a9f1f5
Pablo <pablogroza2000@gmail.com>
2025-05-20 01:40:09 +0200

    Javascript done, tomorrow explanations and CSS

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-const passwordGen = document.querySelector(".password")
-const copyPass = document.querySelector(".copy")
-const btn = document.querySelector(".btn-box")
-const copiedPass = document.querySelector(".copied")
+const passwordGen = document.querySelector(".password");
+const copyPass = document.querySelector(".copy");
+const btn = document.querySelector(".btn-box");
+const copiedPass = document.querySelector(".copied");
+
+function generatePassword() {
+    copiedPass.classList.remove("visible");
+
+    const characters = "QAZ*WS()XEDF{}RC![]?VTG*()BY!HNUJFGMI!?*KOLPq!?a[]*zwsxedc!?rvfv*!?gbych*{}?NUJm?*!ik{)}o!?lp*12!?{13}?!45912376!?}?";
+    const length = 15;
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomChar = characters.charAt(Math.floor(Math.random() * characters.length));
+        password += randomChar;
+    }
+
+    passwordGen.innerText = password;
+}
+
+copyPass.addEventListener("click", () => {
+    if (passwordGen.innerText !== "") {
+        navigator.clipboard.writeText(passwordGen.innerText)
+            .then(() => {
+                copiedPass.classList.add("visible");
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    } else {
+        alert("Nothing to copy...");
+    }
+});
+
+btn.addEventListener("click", generatePassword);
